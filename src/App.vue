9fca46095e4570958ed89dbfb2742fa596586023
ygorverde <ygorverde572@gmail.com>
2021-04-26 20:05:51 -0300

    deploy

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
+		<Header title="Cod3r - Base de Conhecimento" 
+		:hideToggle="!user"
+		:hideUserDropdown="!user"/>
+		<Menu v-if="user"/>
+		<Loading v-if="validatingToken"/>
+		<Content v-else/>	
+		<Footer />
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+import { baseApiUrl, userKey } from '@/global'
+import { mapState } from 'vuex'
+import Header from '@/components/template/Header'
+import Menu from '@/components/template/Menu'
+import Content from '@/components/template/Content'
+import Footer from '@/components/template/Footer'
+import Loading from '@/components/template/Loading'
+// !user se não tiver usuário
+export default {
+	name: "App",
+	components: { Header, Menu, Content, Footer, Loading },
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+			validatingToken: true
+		}
+	},
+	methods: {
+		async validateToken(){
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData){
+				this.validatingToken = false
+				return this.$router.push({ name: 'auth' })
+				// return
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if(res.data){
+				this.$store.commit('setUser', userData)
+				if(this.$mq === 'xs' || this.$mq === 'sm'){
+					this.$store.commit('toggleMenu', false)
+				}
+			}else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validatingToken = false
+		}
+	},
+	created(){
+		this.validateToken() // Antes do Mounted
+	}
+}
+</script>
+
+<style>
+	* {
+		font-family: "Lato", ; /* Referenciada em public/index.html */
+	}
+
+	body {
+		margin: 0px;
+	}
+
+	#app { /* Div que envolverá toda a aplicação */
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 60px; /* 3 Linhas: Header, Content, Footer */
+		grid-template-columns: 300px 1fr; /* 2 Colunas: Navbar(Menu) e o Conteúdo */
+		grid-template-areas:  /* 6 Partes */
+			"header header"
+			"menu content"
+			"menu footer"
+	}
+
+	#app.hide-menu {
+		grid-template-areas:
+		"header header"
+		"content content"
+		"footer footer"
+	}
+</style>
