888f68148f6c17c860174adb97ab9397d80c360a
ygorverde <ygorverde572@gmail.com>
2021-04-30 01:06:34 -0300

    .

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 export const userKey = '__knowledge_user'
-export const baseApiUrl = process.env.VUE_APP_API_URL
+export const baseApiUrl = 'https://knowledgebackend.herokuapp.com'
 
 export function showError(e) {
     if(e && e.response && e.response.data) {
