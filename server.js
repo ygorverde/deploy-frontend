ea3521796e4501f81eb54d9de8a11e5c104ff6ec
ygorverde <ygorverde572@gmail.com>
2021-05-01 01:27:30 -0300

    .

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
-const router = require('express').Router()
 const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.use('/auth', router)
+app.get('*', (req, res) => {
+    res.sendFile(__dirname, '/dist/index.html');
+  });
 
 const port = process.env.PORT || 8080
+const hostname = process.env.VUE_APP_BASEURL
+
+app.listen(port, hostname, () => {
+    console.log("Listening at http://%s:%s/", hostname, port);
+  });
+
 app.listen(port)
 
 console.log('Escutando na porta: ' + port)
