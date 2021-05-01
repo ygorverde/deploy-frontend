76b5440bd6473b2a19cb562f8aa2a9c5340f1c2e
ygorverde <ygorverde572@gmail.com>
2021-05-01 01:20:46 -0300

    ,

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const router = require('express').Router()
 const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/auth', router)
 
 const port = process.env.PORT || 8080
 app.listen(port)
